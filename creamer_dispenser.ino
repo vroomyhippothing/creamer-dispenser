0a326fee2b747b5f43090feee19be545460b8175
joshua1024 <50565600+joshua1024@users.noreply.github.com>
2021-08-26 14:44:22 -0700

    Update creamer_dispenser.ino

--- creamer_dispenser.ino
+++ creamer_dispenser.ino
@@ -4,20 +4,33 @@ const int down = 550;
 const int up = 2400;
 const byte servoPin = 0;
 const byte switchPin = 2;
+const byte ledPin = 1;
 void setup() {
   pinMode(servoPin, OUTPUT);
   pinMode(switchPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   if (digitalRead(switchPin) == LOW) { //pressed
-    digitalWrite(servoPin, HIGH);
-    delayMicroseconds(down);
-    digitalWrite(servoPin, LOW);
+    digitalWrite(ledPin, LOW);
+    for (int i = 0; i < 75; i++) {
+      digitalWrite(servoPin, HIGH);
+      delayMicroseconds(down);
+      digitalWrite(servoPin, LOW);
+      delay(20);//50HZ
+    }
+    for (int i = 0; i < 63; i++) {
+      digitalWrite(servoPin, HIGH);
+      delayMicroseconds(up);
+      digitalWrite(servoPin, LOW);
+      delay(20);//50HZ
+    }
   } else { //not pressed
+    digitalWrite(ledPin, HIGH);
     digitalWrite(servoPin, HIGH);
     delayMicroseconds(up);
     digitalWrite(servoPin, LOW);
+    delay(20);//50HZ
   }
-  delay(20);//50HZ
 }
